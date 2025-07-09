f3bf6c69d729507589f0ecbe5c16ed00d36b073c
Fyoncle <63975256+Fyoncle@users.noreply.github.com>
2025-07-09 21:47:05 +0300

    ⚡ Backport to 1.19.3, 1.19.4

--- src/main/java/net/grindyelytras/item/ModItems.java
+++ src/main/java/net/grindyelytras/item/ModItems.java
@@ -1,21 +1,31 @@
 package net.grindyelytras.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.grindyelytras.GrindyElytras;
 import net.grindyelytras.item.custom.ElytraFragmentItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item ELYTRA_FRAGMENT = register("elytra_fragment",
-            new ElytraFragmentItem(new Item.Settings().group(ItemGroup.TRANSPORTATION)));
+            new ElytraFragmentItem(new Item.Settings()));
 
     private static <I extends Item> I register(String name, I item) {
-        return Registry.register(Registry.ITEM, Identifier.of(GrindyElytras.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, Identifier.of(GrindyElytras.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
         GrindyElytras.LOGGER.info("Grindy Elytras - Torturing people who wants to get Elytra...");
+        init();
+    }
+
+    public static void init() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(modifier -> {
+            modifier.addAfter(Items.ELYTRA, ELYTRA_FRAGMENT);
+        });
     }
 }
