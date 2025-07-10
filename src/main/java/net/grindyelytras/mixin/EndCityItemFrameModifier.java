51b5c87d192ab839737f5de544e4c60737ae75b5
Fyoncle <63975256+Fyoncle@users.noreply.github.com>
2025-07-10 03:11:32 +0300

    🌕 Update to 1.1.0   - Bump Version to 1.1.0   - Use @ModifyArgs instead of @Redirect to avoid conflicts   - Fix Crashes with wrong Method for EndCityGenerator

--- src/main/java/net/grindyelytras/mixin/EndCityItemFrameModifier.java
+++ src/main/java/net/grindyelytras/mixin/EndCityItemFrameModifier.java
@@ -1,24 +1,24 @@
 package net.grindyelytras.mixin;
 
 import net.grindyelytras.item.ModItems;
-import net.minecraft.entity.decoration.ItemFrameEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.structure.EndCityGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(EndCityGenerator.Piece.class)
 public class EndCityItemFrameModifier {
 
-    @Redirect(
-            method = "handleMetadata(Ljava/lang/String;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/ServerWorldAccess;Lnet/minecraft/util/math/random/Random;Lnet/minecraft/util/math/BlockBox;)V",
+    @ModifyArgs(
+            method = "handleMetadata",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;setHeldItemStack(Lnet/minecraft/item/ItemStack;Z)V"
             )
     )
-    private void redirectSetHeldItemStack(ItemFrameEntity itemFrame, ItemStack stack, boolean update) {
-        itemFrame.setHeldItemStack(new ItemStack(ModItems.ELYTRA_FRAGMENT), update);
+    private void modifySetHeldItemStackArgs(Args args) {
+        args.set(0, new ItemStack(ModItems.ELYTRA_FRAGMENT));
     }
 }
